244cf48d5762c8cbec5ed651b75a9bd357329506
SnkaeSael <66127672+SnkaeSael@users.noreply.github.com>
2021-07-28 18:47:23 -0400

    typo

--- smb-inputlua-ntsc-sleek.lua
+++ smb-inputlua-ntsc-sleek.lua
@@ -78,7 +78,7 @@ frameNumbers = {"h", "i", "j", "k", "0", "1", "2", "3", "4", "5", "6", "7", "8",
 
 remainder = ""
 frame = ""
-frameBool = ftrue
+frameBool = true
 
 function drawLetter(x, y, letterData, on)
 	for xo = 0, 2 do
