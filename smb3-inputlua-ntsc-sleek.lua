57b8a7ec09c7213beab53cfd0e7e3b23749a5433
SnkaeSael <66127672+SnkaeSael@users.noreply.github.com>
2021-09-11 22:11:37 -0400

    Updated spacing and toHex functions

--- smb3-inputlua-ntsc-sleek.lua
+++ smb3-inputlua-ntsc-sleek.lua
@@ -109,65 +109,75 @@ function drawStats(x, y)
 	drawText(x+16, y, letterTable("x " .. toHex2(memory.readbyte(0x75)) .. toHex(memory.readbyte(0x90)) .. toHex1(memory.readbyte(0x74d))))
 	drawText(x+16, y +7, letterTable("y " .. toHex2(memory.readbyte(0x87)) .. toHex(memory.readbyte(0xa2)) .. toHex1(memory.readbyte(0x75f))))
 	if(memory.readbyte(0x5fc) > 0 and memory.readbyteunsigned(0x5fc) < 2) then
-	drawText(x+216, y, letterTable("scrnx " .. memory.readbyte(0xab)))
+		drawText(x+216, y, letterTable("scrnx " .. memory.readbyte(0xab)))
 	end
-	--
-	drawText(x+44, y, letterTable("spd "))
-	tempint=memory.readbytesigned(0xbd)
-	if(tempint<0)then
-	drawText(x+60, y, letterTable("x" .. tempint))
+	
+	drawText(x + 44, y, letterTable("spd "))
+	tempint = memory.readbytesigned(0xbd)
+	
+	if (tempint < 0) then
+		drawText(x + 60, y, letterTable("x" .. tempint))
 	else
-	drawText(x+60, y, letterTable("x " .. tempint))
+		drawText(x + 60, y, letterTable("x " .. tempint))
 	end
-	tempint=memory.readbytesigned(0xcf)
-	if(tempint<0)then
-	drawText(x+60, y+7, letterTable("y" .. tempint))
+	
+	tempint = memory.readbytesigned(0xcf)
+	
+	if (tempint < 0)then
+		drawText(x + 60, y + 7, letterTable("y" .. tempint))
 	else
-	drawText(x+60, y+7, letterTable("y " .. tempint))
+		drawText(x + 60, y + 7, letterTable("y " .. tempint))
 	end
-	--
-	if(memory.readbytesigned(0x3dd) > 1) then
+	
+	if (memory.readbytesigned(0x3dd) > 1) then
 		drawText(x+80, y, letterTable("next p " .. math.floor(memory.readbyte(0x515)/8) .. math.fmod(memory.readbyte(0x515), 8)))
 	end
+	
 	if(memory.readbyteunsigned(0x3dd) > 63) then
+	
 	--if(memory.readbyteunsigned(0x56e) == 128) then
-	--pkillsub = 0
+	--	pkillsub = 0
 	--end
 	--if(memory.readbyteunsigned(0x56e) == 0) then
-	--pkillsub = 1
+	--	pkillsub = 1
 	--end
-	if(memory.readbyteunsigned(0x56e) == pkill and memory.readbyteunsigned(0x56e) > 0) then
-	tempint = -1
+	
+	if (memory.readbyteunsigned(0x56e) == pkill and memory.readbyteunsigned(0x56e) > 0) then
+		tempint = -1
 	else
-	tempint = 0
+		tempint = 0
 	end
-	if(memory.readbyteunsigned(0x56e) == 255) then
-	tempint = -511
+	
+	if (memory.readbyteunsigned(0x56e) == 255) then
+		tempint = -511
 	end
-	drawText(x+80, y + 7, letterTable("p kill " .. memory.readbyteunsigned(0x56e) * 2 + tempint))
+		drawText(x + 80, y + 7, letterTable("p kill " .. memory.readbyteunsigned(0x56e) * 2 + tempint))
 	end
+	
 	pkill = memory.readbyteunsigned(0x56e)
-	--
-	drawText(x+124, y, letterTable("frm " .. tostring(emu.framecount())))
-	drawText(x+124, y + 7, letterTable("lag " .. tostring(emu.lagcount())))
+	drawText(x + 124, y, letterTable("frm " .. tostring(emu.framecount())))
+	drawText(x + 124, y + 7, letterTable("lag " .. tostring(emu.lagcount())))
 	--drawText(x, y +105, letterTable("time"))
-	drawText(x+168, y, letterTable(timeCount()))
-	--
-	for i=0, 4, 1 do
-		if((memory.readbyte(0x671+i) == 14 or memory.readbyte(0x671+i) == 24 or memory.readbyte(0x671+i) == 75 or memory.readbyte(0x671+i) == 76) and memory.readbyte(0x661+i) > 0) then
+	drawText(x + 168, y, letterTable(timeCount()))
+	
+	for i = 0, 4 do
+		if ((memory.readbyte(0x671+i) == 14 or memory.readbyte(0x671+i) == 24 or memory.readbyte(0x671+i) == 75 or memory.readbyte(0x671+i) == 76) and memory.readbyte(0x661+i) > 0) then
 			drawText(x+216, y, letterTable("hp " .. memory.readbyte(0x7cf6+i)))
 		end
 	end
-	if(memory.readbyte(0x14) == 1 and backtomap == 0) then
+	
+	if (memory.readbyte(0x14) == 1 and backtomap == 0) then
 		backtomap = 1
-		if(not(memory.readbyte(0x675) == 37 or (memory.readbyte(0x675) == 14 and memory.readbyte(0x7a01) == 0))) then
+		if (not(memory.readbyte(0x675) == 37 or (memory.readbyte(0x675) == 14 and memory.readbyte(0x7a01) == 0))) then
 			levelcount = levelcount + 1
 		end
 	end
-	if(memory.readbyte(0x14) == 0 and backtomap == 1) then
+	
+	if (memory.readbyte(0x14) == 0 and backtomap == 1) then
 		backtomap = 0
 	end
-	drawText(x+216, y+7, letterTable("lvl " .. tostring(levelcount) .. "/104"))
+	
+	drawText(x + 216, y + 7, letterTable("lvl " .. tostring(levelcount) .. "/104"))
 end
 
 function drawText(x, y, arr)
@@ -187,19 +197,15 @@ function letterTable(str)
 end
 
 function toHex(num)
-	local digit1 = hexNumbers[math.floor(num / 16) + 1]
-	local digit2 = hexNumbers[math.fmod(num, 16) + 1]
-	return digit1 .. digit2
+	return string.format('%02x', num)
 end
 
 function toHex1(num)
-	local digit1 = hexNumbers[math.floor(num / 16) + 1]
-	return digit1
+	return hexNumbers[math.floor(num / 16) + 1]
 end
 
 function toHex2(num)
-	local digit2 = hexNumbers[math.fmod(num, 16) + 1]
-	return digit2
+	return hexNumbers[math.fmod(num, 16) + 1]
 end
 
 function timeCount()
