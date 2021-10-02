18ec6141394831c6bae282e81431c6ed619ce0c0
SnkaeSael <66127672+SnkaeSael@users.noreply.github.com>
2021-10-02 13:28:14 -0400

    Updated to InputLua 3

--- smb-inputlua-pal-sleek.lua
+++ smb-inputlua-pal-sleek.lua
@@ -1,4 +1,4 @@
---------------------------- Sleek InputLua for SMB1/2J on FCEUX, PAL -------------------------------
+--------------------------- Sleek InputLua for SMB1/2J on FCEUX, NTSC -------------------------------
 
 inputOrder = {
 	"up",
@@ -7,121 +7,177 @@ inputOrder = {
 	"right",
 	"start",
 	"select",
-	"A",
-	"B"
+	"B",
+	"A"
 }
 
-display = {A = "010101111101101", B = "110101110101110", select = "011100010001110", start = "111010010010010", up = "101101101101111", down = "110101101101110", left = "100100100100111", right = "110101110101101"}
+display = {A = "a", B = "b", select = "s", start = "t", up = "u", down = "d", left = "l", right = "r"}
 
-nondisplay = {
-	"010101111101101",
-	"110101110101110",
-	"011100100100011",
-	"110101101101110",
-	"111100111100111",
-	"111100111100100",
-	"011100101101011",
-	"101101111101101",
-	"111010010010111",
-	"011001001101011",
-	"101101110101101",
-	"100100100100111",
-	"101111101101101",
-	"110101101101101",
-	"010101101101010",
-	"110101111100100",
-	"010101101111011",
-	"110101110101101",
-	"011100010001110",
-	"111010010010010",
-	"101101101101111",
-	"101101101101010",
-	"101101101111101",
-	"101101010101101",
-	"101101010010010",
-	"111001010100111",
-	"111101101101111",
-	"010110010010111",
-	"110001010100111",
-	"110001110001110",
-	"101101111001001",
-	"111100110001110",
-	"111100111101111",
-	"111001010010010",
-	"111101111101111",
-	"111101111001111",
-	"000000000000000",
-	"000000000000010",
-	"000000111000000",
-	"000010000000010"
+charIndex = {
+	a = "010101111101101",
+	b = "110101110101110",
+	c = "011100100100011",
+	d = "110101101101110",
+	e = "111100111100111",
+	f = "111100111100100",
+	g = "011100101101011",
+	h = "101101111101101",
+	i = "111010010010111",
+	j = "011001001101011",
+	k = "101101110101101",
+	l = "100100100100111",
+	m = "101111101101101",
+	n = "110101101101101",
+	o = "010101101101010",
+	p = "110101111100100",
+	q = "010101101111011",
+	r = "110101110101101",
+	s = "011100010001110",
+	t = "111010010010010",
+	u = "101101101101111",
+	v = "101101101101010",
+	w = "101101101111101",
+	x = "101101010101101",
+	y = "101101010010010",
+	z = "111001010100111"
 }
 
-blackscrninc = false
+charIndex["0"] = "111101101101111"
+charIndex["1"] = "010110010010111"
+charIndex["2"] = "110001010100111"
+charIndex["3"] = "110001110001110"
+charIndex["4"] = "101101111001001"
+charIndex["5"] = "111100110001110"
+charIndex["6"] = "111100111101111"
+charIndex["7"] = "111001010010010"
+charIndex["8"] = "111101111101111"
+charIndex["9"] = "111101111001111"
+charIndex[" "] = "000000000000000"
+charIndex["."] = "000000000000010"
+charIndex["-"] = "000000111000000"
+charIndex[":"] = "000010000000010"
 
-letterIndex = {a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21, v = 22, w = 23, x = 24, y = 25, z = 26}
-
-letterIndex["0"] = 27
-letterIndex["1"] = 28
-letterIndex["2"] = 29
-letterIndex["3"] = 30
-letterIndex["4"] = 31
-letterIndex["5"] = 32
-letterIndex["6"] = 33
-letterIndex["7"] = 34
-letterIndex["8"] = 35
-letterIndex["9"] = 36
-letterIndex[" "] = 37
-letterIndex["."] = 38
-letterIndex["-"] = 39
-letterIndex[":"] = 40
-
-hexNumbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"}
+input = ""
+--hexNumbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"}
 frameNumbers = {"e", "f", "g", "h", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d"}
 
+blackscrninc = false
 remainder = ""
 frame = ""
 frameBool = true
+colour = "white"
 
-function drawLetter(x, y, letterData, on)
+function drawLetter(x, y, letterData)
 	for xo = 0, 2 do
 		for yo = 0, 4 do
 			local stringOffset = yo * 3 + xo + 1
-			if (string.sub(letterData, stringOffset, stringOffset) == "1" and on) then
-				gui.pixel(x + xo, y + yo, "white")
+			if (string.sub(letterData, stringOffset, stringOffset) == "1") then
+				gui.pixel(x + xo, y + yo, colour)
 			end
 		end
 	end
 end
 
-function drawInput(x, y)
+function inputStr()
+	input = ""
+	
 	for i = 1, 8 do
-		if (input[inputOrder[i]]) then
-			drawLetter(x + (4 * (i - 1)), y, display[tostring(inputOrder[i])], true, false)
+		if (joypad.get(1)[inputOrder[i]]) then
+			input = input .. display[inputOrder[i]]
+		else
+			input = input .. " "
 		end
 	end
 end
 
-function drawStats(x, y)
-	drawText(x, y, letterTable("xp " .. toHex(memory.readbyte(109)) .. toHex(memory.readbyte(134)) .. toHex(memory.readbyte(1024)) .. " xs " .. memory.readbytesigned(87) .. ":" .. toHex(memory.readbyte(1797))))
-	drawText(x + 80, y, letterTable("sx " .. memory.readbyte(941)))
-	drawText(x, y + 7, letterTable("yp " .. toHex(memory.readbyte(181)) .. toHex(memory.readbyte(206)) .. toHex(memory.readbyte(1046)) .. " ys " .. memory.readbytesigned(159) .. ":" .. toHex(memory.readbyte(1075))))
-	drawText(x + 80, y + 7, letterTable("fr " .. math.fmod(math.floor((emu.framecount() - emu.lagcount() - 1) / 18), 32767)))
-	drawText(x + 116, y, letterTable("f " .. memory.readbyte(9)))
-	drawText(x + 116, y + 7, letterTable("r " .. remainder))
-	drawText(x + 140, y, letterTable("s " .. sock()))
-	drawText(x + 140, y + 7, letterTable("-" .. frame))
-	drawText(x - 52, y + 7, letterTable(doubleDigit(timeCount()[1]) .. ":" .. doubleDigit(timeCount()[2]) .. ":" .. doubleDigit(timeCount()[3])))
-	drawText(x - 52, y, letterTable("inp"))
-	drawText(x + 152, y + 7, letterTable("lag " .. emu.lagcount()))
-	if (memory.readbyte(22) == 45 or memory.readbyte(23) == 45 or memory.readbyte(24) == 45 or memory.readbyte(25) == 45 or memory.readbyte(26) == 45) then
-		drawText(x + 176, y, letterTable("bhp " .. memory.readbyte(1155)))
+function readmemory()
+	state = memory.readbyte(14)
+	framerule = memory.readbyte(1919)
+	levelendtimer = {memory.readbyte(1942), memory.readbyte(1943), memory.readbyte(1944), memory.readbyte(1945), memory.readbyte(1946)}
+	blackscreentimer = memory.readbyte(1952)
+	castletimer = memory.readbyte(1953)
+	
+	xpage = memory.readbyte(109)
+	xpixel = memory.readbyte(134)
+	xsubpx = memory.readbyte(1024)
+	xspd = memory.readbytesigned(87)
+	xsubspd = memory.readbyte(1797)
+	
+	scrnx = memory.readbyte(941)
+	
+	ypage = memory.readbyte(181)
+	ypixel = memory.readbyte(206)
+	ysubpx = memory.readbyte(1046)
+	yspd = memory.readbytesigned(159)
+	ysubspd = memory.readbyte(1075)
+	
+	igframe = memory.readbyte(9)
+	
+	bowserhp = memory.readbyte(1155)
+	
+	enemyslot = {memory.readbyte(22), memory.readbyte(23), memory.readbyte(24), memory.readbyte(25), memory.readbyte(26)}
+end
+
+function updateTimers()
+	if (state == 0) then
+		if (blackscreentimer == 7 and blackscreeninc) then
+			remainder = doubleDigit(framerule)
+			blackscreeninc = false
+		end
+		
+		if (frameBool) then
+			frame = frameNumbers[framerule + 1]
+			frameBool = false
+		end
+	else
+		frameBool = true
+		
+		if (state == 3) then
+			blackscreeninc = true
+		end
+		
+		if (state == 5 and (levelendtimer[1] == 6 or levelendtimer[2] == 6 or levelendtimer[3] == 6 or levelendtimer[4] == 6 or levelendtimer[5] == 6) and remainder == "") then
+			remainder = doubleDigit(framerule)
+			blackscreeninc = true
+		end
+		
+		if (castletimer == 6) then
+			remainder = doubleDigit(framerule)
+		end
+		
+		if (state == 7) then
+			remainder = ""
+		end
+		
+		if (state == 8) then
+			blackscreeninc = false
+		end
 	end
 end
 
-function drawText(x, y, arr)
+function drawStats()
+	drawText(2, 2, "inp" .. input)
+	drawText(2, 9, doubleDigit(timeCount()[1]) .. ":" .. doubleDigit(timeCount()[2]) .. ":" .. doubleDigit(timeCount()[3]))
+	drawText(56, 2, "xp " .. toHex(xpage) .. toHex(xpixel) .. toHex(xsubpx) .. " xs " .. xspd .. ":" .. toHex(xsubspd))
+	drawText(136, 2, "sx " .. scrnx)
+	drawText(56, 9, "yp " .. toHex(ypage) .. toHex(ypixel) .. toHex(ysubpx) .. " ys " .. yspd .. ":" .. toHex(ysubspd))
+	drawText(136, 9, "fr " .. math.fmod(math.floor((emu.framecount() - emu.lagcount() - 1) / 18), 32767))
+	drawText(172, 2, "f " .. igframe)
+	drawText(172, 9, "r " .. remainder)
+	drawText(196, 2, "s " .. sock())
+	drawText(196, 9, "-" .. frame)
+	drawText(208, 9, "lag " .. emu.lagcount())
+	
+	if (enemyslot[1] == 45 or enemyslot[2] == 45 or enemyslot[3] == 45 or enemyslot[4] == 45 or enemyslot[5] == 45) then
+		drawText(232, 2, "bhp " .. bowserhp)
+	end
+end
+
+function drawText(x, y, lt)
+	local arr = letterTable(lt)
 	local l = #arr
 	for xo = 1, l do
-		drawLetter(x + (xo - 1) * 4, y, nondisplay[arr[xo]], true, false)
+		drawLetter(x + (xo - 1) * 4, y, arr[xo])
 	end
 end
 
@@ -129,7 +185,7 @@ function letterTable(str)
 	local l = #str
 	local t = {}
 	for i = 1, l do
-		t[i] = letterIndex[string.sub(str, i, i)]
+		t[i] = charIndex[string.sub(str, i, i)]
 	end
 	return t
 end
@@ -144,39 +200,6 @@ function timeCount()
 	local m = math.fmod(math.floor(t / 60), 60)
 	local s = round(math.fmod(t, 60) * 1000) / 1000
 	return {h, m, s}
-end
-
-function updateTimers()
-	if (memory.readbyte(14) == 0) then
-		if(memory.readbyte(1952) == 7 and blackscreeninc) then
-			remainder = doubleDigit(memory.readbyte(1919))
-			blackscreeninc = false
-		end
-		
-		if (frameBool) then
-			frame = frameNumbers[memory.readbyte(1919) + 1]
-			frameBool = false
-		end
-	else
-		frameBool = true
-		
-		if (memory.readbyte(14) == 3 and remainder == "") then
-			remainder = doubleDigit(memory.readbyte(1919))
-		end
-		
-		if (memory.readbyte(14) == 5 and (memory.readbyte(1942) == 6 or memory.readbyte(1943) == 6 or memory.readbyte(1944) == 6 or memory.readbyte(1945) == 6 or memory.readbyte(1946) == 6) and remainder == "") then
-			remainder = doubleDigit(memory.readbyte(1919))
-			blackscreeninc = true
-		end
-		
-		if (memory.readbyte(1953) == 6) then
-			remainder = doubleDigit(memory.readbyte(1919))
-		end
-		
-		if (memory.readbyte(14) == 7) then
-			remainder = ""
-		end
-	end
 end
 
 function doubleDigit(num)
@@ -206,10 +229,10 @@ function sock()
 end
 
 function drawLua()
-	input = joypad.get(1)
+	inputStr()
+	readmemory()
 	updateTimers()
-	drawInput(18, 2)
-	drawStats(54, 2)
+	drawStats()
 end
 
 while (true) do
